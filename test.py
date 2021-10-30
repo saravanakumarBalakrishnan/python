524c30831d3954b14a72ff60b354c029de1c872c
SaravanaKumarBalakrishnan <bsaravanakumars952@gmail.com>
2021-10-30 21:24:06 +0100

    update test.py

--- test.py
+++ test.py
@@ -1,7 +1,43 @@
-import time
 
-time.sleep(1)
-print('hello')
-time.sleep(1)
-print('world')
-print(" Added new file and build successfull")
+import sys
+import glob
+import serial
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/ttyUSB*') # ubuntu is /dev/ttyUSB0
+    elif sys.platform.startswith('darwin'):
+        # ports = glob.glob('/dev/tty.*')
+        ports = glob.glob('/dev/tty.SLAB_USBtoUART*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            print("Successfully Found Serial Port\n")
+            result.append(port)
+        except serial.SerialException as e:
+            if e.errno == 13:
+                raise e
+            pass
+        except OSError:
+            pass
+    return result
+
+
+if __name__ == '__main__':
+    print(serial_ports())
+
